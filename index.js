d540dc97682db2a94810bfafe893a1a1234c1bb5
Obongiyakke Udom <udomobongiyakke@gmail.com>
2021-07-30 15:22:51 +1200

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+function openFunction(){
+    document.getElementById("menu").style.width="300px";
+    document.getElementById("mainbox").style.marginLeft="300px";
+    // document.getElementById("mainbox").innerHTML="Click On Cross Element And Close Menu";
+}
+function closeFunction(){
+    document.getElementById("menu").style.width="0px";
+    document.getElementById("mainbox").style.marginLeft="0px";
+    // document.getElementById("mainbox").innerHTM="&#9776; Open";
+}
